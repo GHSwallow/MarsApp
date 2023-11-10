006b723edf147a7cf0bd1377e5429a0f05ba3857
GHSwallow <Gabriel.Swallow@softwire.com>
2023-11-10 15:29:17 +0000

    backend endpoints for rovers, rover and pictures

---
+++ src/server.js
@@ -0,0 +1,34 @@
+import express from "express";
+import * as dotenv from "dotenv";
+import './rovers.js'
+import * as NASAActions from "./rovers.js";
+
+
+const app = express();
+const port = 8000;
+
+if (process.env.NODE_ENV !== "production"){
+    dotenv.config();
+}
+
+app.use(express.json());
+const router = express.Router();
+app.use('/', router);
+app.listen(port, () => {
+    console.log(`Backend is running on port ${port}`);
+});
+
+router.get('/rovers',
+    async (req, res) =>
+        res.send(await NASAActions.getRoverNames())
+);
+
+router.get('/rovers/:rover',
+    async (req, res) =>
+        res.send(await NASAActions.getSpecificRoverData(req.params.rover))
+);
+
+router.get('/rovers/:rover/pictures/:camera',
+    async (req, res) =>
+        res.send(await NASAActions.getRoverPhotosByCamera(req.params.rover, req.params.camera))
+);
